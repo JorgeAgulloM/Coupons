4fded0a107c6540b994aa7c9eacbf6dd4082efba
Jorge Agullo <agullojorge@gmail.com>
2022-04-18 16:23:19 +0200

    Added interface DAO

---
+++ app/src/main/java/com/example/coupons/CouponDao.kt
@@ -0,0 +1,15 @@
+package com.example.coupons
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface CouponDao {
+
+    @Query("SELECT * FROM CouponEntity WHERE code = code")
+    suspend fun consultCouponByCode(code: String): CouponEntity?
+
+    @Insert
+    suspend fun addCoupon(couponEntity: CouponEntity): Long
+}
