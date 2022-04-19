d7c8dac4fdea2faaafc3b51feeda055a8acb83cb
Jorge Agullo <agullojorge@gmail.com>
2022-04-19 11:51:43 +0200

    Edited Repo

--- app/src/main/java/com/example/coupons/mainModule/model/MainRepository.kt
+++ app/src/main/java/com/example/coupons/mainModule/model/MainRepository.kt
@@ -1,4 +1,20 @@
 package com.example.coupons.mainModule.model
 
+import com.example.coupons.common.entities.CouponEntity
+import com.example.coupons.common.utils.Constants
+import com.example.coupons.common.utils.validateTxtCode
+import java.lang.Exception
+
 class MainRepository {
+    private val roomDataBase = RoomDataBase()
+
+    suspend fun consultCouponByCode(code: String) = roomDataBase.consultCouponByCode(code)
+
+    suspend fun saveCoupon(couponEntity: CouponEntity) {
+        if (validateTxtCode(couponEntity.code)) {
+            roomDataBase.saveCoupon(couponEntity)
+        } else {
+            throw Exception(Constants.ERROR_LENGTH)
+        }
+    }
 }
