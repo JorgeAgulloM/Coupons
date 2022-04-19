a06dacb42acddbabcc25155ede15287665882558
Jorge Agullo <agullojorge@gmail.com>
2022-04-19 16:48:10 +0200

    Change welcome text if condition

--- app/src/main/java/com/example/coupons/common/adapters/CouponBindingAdapter.kt
+++ app/src/main/java/com/example/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,9 +1,24 @@
 package com.example.coupons.common.adapters
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.example.coupons.R
 
 @BindingAdapter("isGone")
-fun bindIsGone(view: View, isGone: Boolean) {
-    view.visibility = if (isGone) View.GONE else View.VISIBLE
-}
+    fun bindIsGone(view: View, isGone: Boolean) {
+        view.visibility = if (isGone) View.GONE else View.VISIBLE
+    }
+
+    @BindingAdapter("textTitle")
+    fun bindIsActive(view: TextView, isActive: Boolean) {
+        if (isActive) {
+            view.setText(R.string.main_text_title)
+            view.setTextColor(Color.BLACK)
+        } else {
+            view.setText(R.string.main_text_available)
+            view.setTextColor(Color.BLUE)
+        }
+    }
+
